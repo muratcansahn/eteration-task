5356d3a4e7b531ac9e5756f8a856349a9bb1a4ff
muratcansahn <muratcansahin@192.168.1.5>
2024-05-24 23:43:10 +0300

    feat:add react router dom && create dynamic product detail pages

--- src/pages/ProductDetailPage/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage/ProductDetailPage.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
+
 
 const ProductDetailPage = () => {
+    const { id } = useParams();
+    console.log(id)
+
   return (
-    <div>ProductDetailPage</div>
+    <div>{id}   </div>
   )
 }
 
