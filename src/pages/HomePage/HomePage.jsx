5fb09d05a4f91e6901e816fcf3bbd4ec166ac71d
muratcansahn <muratcansahin@192.168.1.5>
2024-05-26 22:08:05 +0300

    feat:add spinner on load

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,91 +7,101 @@ import { setAutoCompleteOptions } from '../../redux/autoCompleteOptionsSlice';
 import { setSearchQuery } from '../../redux/searchQuerySlice'; 
 import useIsMobile from "../../hooks/useIsMobile";
 import MobileDrawer from "../../components/MobileDrawer/MobileDrawer";
+import { Spin } from "antd";
 
 const HomePage = () => {
-  const [value, setValue] = useState(1);
-  const [products, setProducts] = useState([]);
-  const [filteredProducts, setFilteredProducts] = useState([]);
-  const [selectedTags, setSelectedTags] = useState([]);
-  const dispatch = useDispatch();
-  const autoCompleteOptions = useSelector(state => state.autoCompleteOptions.autoCompleteOptions);
-  const searchQuery = useSelector(state => state.searchQuery.searchQuery);
+    const [value, setValue] = useState(1);
+    const [products, setProducts] = useState([]);
+    const [filteredProducts, setFilteredProducts] = useState([]);
+    const [selectedTags, setSelectedTags] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const dispatch = useDispatch();
+    const autoCompleteOptions = useSelector((state) => state.autoCompleteOptions.autoCompleteOptions);
+    const searchQuery = useSelector((state) => state.searchQuery.searchQuery);
 
-  useEffect(() => {
-    // Fetch products from the API
-    fetch("https://5fc9346b2af77700165ae514.mockapi.io/products")
-        .then((response) => response.json())
-        .then((data) => {
-            setProducts(data);
-            setFilteredProducts(data);
-            // Extract tags for autoCompleteOptions
-            const tags = data.map((product) => product.name);
-            const autoCompleteOptionsTag = tags.map((tag) => ({ value: tag }));
-            dispatch(setAutoCompleteOptions(autoCompleteOptionsTag));
-        })
-        .catch((error) => console.error("Error fetching products:", error));
-  }, [dispatch]);
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {
+            fetch("https://5fc9346b2af77700165ae514.mockapi.io/products")
+                .then((response) => response.json())
+                .then((data) => {
+                    setProducts(data);
+                    setFilteredProducts(data);
+                    const tags = data.map((product) => product.name);
+                    const autoCompleteOptionsTag = tags.map((tag) => ({ value: tag }));
+                    dispatch(setAutoCompleteOptions(autoCompleteOptionsTag));
+                })
+                .catch((error) => console.error("Error fetching products:", error))
+                .finally(() => setLoading(false));
+        }, 1000);
+    }, []);
 
-  const onChange = (e) => {
-    setValue(e.target.value);
-  };
+    const onChange = (e) => {
+        setValue(e.target.value);
+    };
 
-  useEffect(() => {
-    if (value === "oldtoNew") {
-      setFilteredProducts([...filteredProducts].sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt)));
-    }
-    if (value === "newtoOld") {
-      setFilteredProducts([...filteredProducts].sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)));
-    }
-    if (value === "priceLowtoHigh") {
-      setFilteredProducts([...filteredProducts].sort((a, b) => a.price - b.price));
-    }
-    if (value === "priceHightoLow") {
-      setFilteredProducts([...filteredProducts].sort((a, b) => b.price - a.price));
-    }
-  }, [value]);
+    useEffect(() => {
+        if (value === "oldtoNew") {
+            setFilteredProducts([...filteredProducts].sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt)));
+        }
+        if (value === "newtoOld") {
+            setFilteredProducts([...filteredProducts].sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)));
+        }
+        if (value === "priceLowtoHigh") {
+            setFilteredProducts([...filteredProducts].sort((a, b) => a.price - b.price));
+        }
+        if (value === "priceHightoLow") {
+            setFilteredProducts([...filteredProducts].sort((a, b) => b.price - a.price));
+        }
+    }, [value]);
 
-  useEffect(() => {
-    if (searchQuery) {
-      setFilteredProducts([
-        ...products.filter(product => product.name.toLowerCase().includes(searchQuery.toLowerCase()))
-      ]);
-    }
-  }, [searchQuery, products]);
+    useEffect(() => {
+        if (searchQuery) {
+            setFilteredProducts([
+                ...products.filter((product) => product.name.toLowerCase().includes(searchQuery.toLowerCase())),
+            ]);
+        }
+    }, [searchQuery, products]);
 
-  useEffect(() => {
-    if (selectedTags.length > 0) {
-      setFilteredProducts([...products].filter(product => selectedTags.includes(product.name)));
-    } else {
-      setFilteredProducts([...products]);
-    }
-  }, [selectedTags, products]);
-const isMobile = useIsMobile();
- 
-  return (
-      <div className={`d-flex home-page-container ${isMobile ? "flex-column" : ""}`}>
-          {isMobile ? (
-              <MobileDrawer
-                  value={value}
-                  onChange={onChange}
-                  autoCompleteOptions={autoCompleteOptions}
-                  setSearchQuery={(query) => dispatch(setSearchQuery(query))}
-                  setSelectedTags={setSelectedTags}
-                  selectedTags={selectedTags}
-              />
-          ) : (
-              <SortandFilterBar
-                  onChange={onChange}
-                  autoCompleteOptions={autoCompleteOptions}
-                  setSearchQuery={(query) => dispatch(setSearchQuery(query))}
-                  value={value}
-                  setSelectedTags={setSelectedTags}
-                  selectedTags={selectedTags}
-              />
-          )}
-          <ProductList products={filteredProducts} />
-      </div>
-  );
-}
+    useEffect(() => {
+        if (selectedTags.length > 0) {
+            setFilteredProducts([...products].filter((product) => selectedTags.includes(product.name)));
+        } else {
+            setFilteredProducts([...products]);
+        }
+    }, [selectedTags, products]);
+    const isMobile = useIsMobile();
+
+    return (
+        <div className={`d-flex home-page-container ${isMobile ? "flex-column" : ""}`}>
+            {isMobile ? (
+                <MobileDrawer
+                    value={value}
+                    onChange={onChange}
+                    autoCompleteOptions={autoCompleteOptions}
+                    setSearchQuery={(query) => dispatch(setSearchQuery(query))}
+                    setSelectedTags={setSelectedTags}
+                    selectedTags={selectedTags}
+                />
+            ) : (
+                <SortandFilterBar
+                    onChange={onChange}
+                    autoCompleteOptions={autoCompleteOptions}
+                    setSearchQuery={(query) => dispatch(setSearchQuery(query))}
+                    value={value}
+                    setSelectedTags={setSelectedTags}
+                    selectedTags={selectedTags}
+                />
+            )}
+            {loading ? (
+                <div className="spin-wrapper">
+                    <Spin size="large" />
+                </div>
+            ) : (
+                <ProductList products={filteredProducts} />
+            )}
+        </div>
+    );
+};
 
 export default HomePage;
--- src/pages/ProductDetailPage/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage/ProductDetailPage.jsx
@@ -5,18 +5,24 @@ import Button from '../../components/Global/Button/Button';
 import { addToCart } from '../../redux/cartSlice';
 import { useDispatch } from 'react-redux';
 import { toast } from "react-toastify";
+import { Spin } from "antd";
 
 const ProductDetailPage = () => {
-  const [product, setProduct] = useState({});
-  useEffect(() => {
-    // Fetch product from the API
-    fetch(`https://5fc9346b2af77700165ae514.mockapi.io/products/${id}`)
-      .then(response => response.json())
-      .then(data => {
-       setProduct(data)
-      })
-      .catch(error => console.error('Error fetching product:', error));
-  }, []);
+    const [product, setProduct] = useState({});
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {
+            // Fetch product from the API
+            fetch(`https://5fc9346b2af77700165ae514.mockapi.io/products/${id}`)
+                .then((response) => response.json())
+                .then((data) => {
+                    setProduct(data);
+                })
+                .catch((error) => console.error("Error fetching product:", error));
+            setLoading(false);
+        }, 1000);
+    }, []);
     const { id } = useParams();
     const dispatch = useDispatch();
     const handleAddToCart = () => {
@@ -24,20 +30,30 @@ const ProductDetailPage = () => {
         toast.success("Product added to cart");
     };
     return (
-        <div className="product-detail-wrapper">
-            <div className="h-100 w-100">
-                <img src={product.image} alt={product.name} className="product-detail-image" />
-            </div>
-            <div className="h-100 w-100 ms-3">
-                <div className="d-flex flex-column">
-                    <h4 className="product-title">{product.name}</h4>
-                    <span className="product-price">{product.price}₺</span>
-                    <Button text="Add to Cart" className={"pe-4 mt-4"} onClick={handleAddToCart} />
+        <>
+            {loading ? (
+                <div className="spin-wrapper">
+                    <Spin size="large" />
                 </div>
-                <p className="product-description py-3">{product.description && product.description.slice(0, 400)}</p>
-            </div>
-        </div>
+            ) : (
+                <div className="product-detail-wrapper">
+                    <div className="h-100 w-100">
+                        <img src={product.image} alt={product.name} className="product-detail-image" />
+                    </div>
+                    <div className="h-100 w-100 ms-3">
+                        <div className="d-flex flex-column">
+                            <h4 className="product-title">{product.name}</h4>
+                            <span className="product-price">{product.price}₺</span>
+                            <Button onClick={handleAddToCart} className="mt-3" text="Add to Cart" />
+                        </div>
+                        <p className="product-description py-3">
+                            {product.description && product.description.slice(0, 400)}
+                        </p>
+                    </div>
+                </div>
+            )}
+        </>
     );
-}
+};
 
 export default ProductDetailPage
